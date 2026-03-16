a03a954be9f8b74ac6dcbcab301a89b281708c53
Manan Jethwa <manan.jethwa.29004@gmail.com>
2026-03-16 15:55:18 +0530

    vite.js file updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   preview: {
     host: true,
     port: process.env.PORT || 3000,
-    allowedHosts: [
-      "mightbee-frontend-production.up.railway.app"
-    ]
+    allowedHosts: true
   }
-})
+});
