cfa1fe26dabf5d3d787196d260751f025b008f34
Manan Jethwa <manan.jethwa.29004@gmail.com>
2026-03-16 15:51:34 +0530

    changes done

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: { "/api": { target: "http://localhost:5000", changeOrigin: true } },
-  },
-});
+  preview: {
+    host: true,
+    port: process.env.PORT || 3000,
+    allowedHosts: [
+      "mightbee-frontend-production.up.railway.app"
+    ]
+  }
+})
